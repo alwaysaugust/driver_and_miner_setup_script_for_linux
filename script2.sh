8a99e94ae41a7bda2ccf15a82090b3af401efab5
Saeid <saeid@alwaysaugust.co>
2019-01-07 17:56:36 +0000

    added software-properties-common

--- script2.sh
+++ script2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # August Miner Setup Prototype
 
+echo "Installing software-properties-common package"
+sudo apt-get install software-properties-common 
+clear
 echo "Adding the PPA for graphic drivers"
 sudo apt-add-repository ppa:graphics-drivers/ppa
 clear
@@ -15,4 +18,7 @@ sudo apt install -y nvidia-396 nvidia-cuda-toolkit
 clear
 echo "Creating NVIDIA config file"
 sudo nvidia-xconfig -a --cool-bits=31 --allow-empty-initial-configuration
+clear
+echo "Downloading the equihash miner"
+wget https://github.com/sbayegan/euihashminer/blob/master/EWBF_Equihash_miner_v0.6_cuda8/miner?raw=true -O equihashminer
 clear
