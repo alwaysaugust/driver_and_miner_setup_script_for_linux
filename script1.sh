fda28e92839040b654a1fec5ae9a2e1be86a2ef1
Saeid <saeid@alwaysaugust.co>
2019-01-07 15:08:28 +0000

    first commit

---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# August Miner Setup Prototype
+
+echo "Updating packages"
+sudo apt-get update 
+clear
+echo "Installing kernel headers"
+sudo apt-get install linux-headers-$(uname -r)
+clear
+echo "Installing xorg-dev"
+sudo apt-get install xorg-dev 
+clear
+echo "Rebooting"
+sudo reboot now
---
+++ script2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# August Miner Setup Prototype
+
+echo "Adding the PPA for graphic drivers"
+sudo apt-add-repository ppa:graphics-drivers/ppa
+clear
+echo "Updating the APT to add the newly added PPA"
+sudo apt-get update
+clear
+echo "Installing the drivers"
+sudo apt install -y nvidia-396 nvidia-cuda-toolkit
+clear
+echo "Installing CUDA"
+sudo apt install -y nvidia-396 nvidia-cuda-toolkit
+clear
+echo "Creating NVIDIA config file"
+sudo nvidia-xconfig -a --cool-bits=31 --allow-empty-initial-configuration
+clear
