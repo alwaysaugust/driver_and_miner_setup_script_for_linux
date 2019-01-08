6370e02cf49b34b3a52ff69d87e2a0a7cfe8c818
Saeid Bayeganeh <saeid@alwaysaugust.co>
2019-01-08 17:04:12 +0000

    script1.sh edited online with Bitbucket

--- script1.sh
+++ script1.sh
@@ -7,6 +7,12 @@ clear
 echo "Installing gcc"
 sudo apt-get install gcc
 clear
+echo "Installing make"
+sudo apt-get install make
+clear
+echo "Installing g++"
+sudo apt-get install g++
+clear
 echo "Installing kernel headers"
 sudo apt-get install linux-headers-$(uname -r)
 clear
